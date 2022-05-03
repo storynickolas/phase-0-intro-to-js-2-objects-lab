106c023e08669abd1a83876e7315ca552c6d0322
Nick Story <storynickolasl@gmail.com>
2022-05-03 08:38:02 -0400

    commit tree 72799a8e0925d5cab64c90097f55057b21402680

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+let employee = {
+  name: 'Oscar',
+  streetAddress: '123 Sesame Street'
+}
+
+function updateEmployeeWithKeyAndValue(employee, key1, value) {
+  let newEmployee = {...employee}
+  newEmployee[key1] = value;
+  return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key2, value) {
+  employee[key2] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key3) {
+  let newEmployee = {...employee}
+  delete newEmployee[key3]
+  return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key4) {
+  delete employee[key4]
+  return employee
+}
